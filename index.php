fd090bbfbf2ac01111cbe6375c1c2d0f0d126186
DESKTOP-7L1NB21\gkuil <99040077@mydavinci.nl>
2018-02-06 10:07:30 +0100

    goede-php Gary Kuiler

--- index.php
+++ index.php
@@ -3,8 +3,43 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
+	<style>
+	main {
+		color: white;
+		text-align: center;
+		padding-top: 300px;
+		font-family: arial;
+	}
+
+	</style>
 </head>
 <body>
-	
+	<main>
+		<h1><?php timeMsg() ?></h1>
+	</main>
+	<?php
+		function timeMsg(){
+			date_default_timezone_set("Europe/Amsterdam");
+			/*echo date_default_timezone_get() . " " . date("H:i");*/
+			$currentHour = date("H");
+			$time = date("H:i");
+
+			if ($currentHour < "12") {
+				echo "<p>Goede Morgen!</p><p>Het is nu " . $time . "</p>";
+				echo "<body style='background: url(backgrounds/morning.png);'>";
+			} elseif ($currentHour < "18") {
+				echo "<p>Goede Middag!</p><p>Het is nu " . $time . "</p>";
+				echo "<body style='background: url(backgrounds/afternoon.png);'>";
+			} elseif ($currentHour < "24") {
+				echo "<p>Goede Avond!</p><p>Het is nu " . $time . "</p>";
+				echo "<body style='background: url(backgrounds/evening.png);'>";
+			} elseif ($currentHour < "6") {
+				echo "<p>Goede Nacht!</p><p>Het is nu " . $time . "</p>";
+				echo "<body style='background: url(backgrounds/night.png);'>";
+			}	else {
+				echo "No valid Time has been found";
+			}
+		}		
+	?>
 </body>
 </html>
